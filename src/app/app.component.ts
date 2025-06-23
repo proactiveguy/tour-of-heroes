8dbed9c976964a504a3071f08f7eba801806c293
proactiveguy <proactiveguy7@gmail.com>
2025-06-23 13:27:05 -0400

    add hero detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, NgFor, NgIf, UpperCasePipe],
+  imports: [FormsModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
